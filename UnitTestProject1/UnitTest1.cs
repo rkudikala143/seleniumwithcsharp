a99c319e4a6c3575f14a4a3d1500bd888a27e132
SeleniumProject <seleniumden@gmail.com>
2023-02-01 16:51:59 +0530

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver _driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            
+
+            _driver = new ChromeDriver("@C:\\chrome\\chromedriver");
+            _driver.Navigate().GoToUrl("https://demowebshop.tricentis.com/");
+            _driver.Manage().Window.Maximize();
+
+        }
+        [TestCategory("Smoke")]
+        [TestMethod]
+        public void VerifyzRegisterPageTitle()
+        {
+            _driver.FindElement(By.ClassName("ico-register")).Click();
+            NUnit.Framework.Assert.AreEqual(_driver.Title, "Demo Web Shop. Register");
+        }
+
+        [TestCategory("Regression")]
+        [TestMethod]
+        public void VerifyLoginPageTitle()
+        {
+            _driver.FindElement(By.ClassName("ico-login")).Click();
+            NUnit.Framework.Assert.AreEqual(_driver.Title, "Demo Web Shop. Login");
+        }
+        [TestCategory("Functional")]
+        [TestMethod]
+        public void VerifyForgotPasswordTitle()
+        {
+            _driver.FindElement(By.ClassName("ico-login")).Click();
+            NUnit.Framework.Assert.AreEqual(_driver.Title, "Demo Web Shop. Login");
+        }
+        [TestCleanup]
+        public void TearDown()
+        {
+            _driver.Close();
+            _driver.Quit();
+        }
+    }
+}
